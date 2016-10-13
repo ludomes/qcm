b336abf61a685f478ac67c9fb47b5a210f9f9a52
messuvel <messuvel@vm-lme-dev>
2016-10-13 17:05:37 +0200

    maj

--- import.php
+++ import.php
@@ -1,12 +1,36 @@
 <?php
-	//Créer un dossier 'import'
-	if (($_FILES['mon_fichier']['name'] != '')) {
-		$filenameImport = md5(uniqid(rand(), true));
-		$dossierImport = 'import';
-		if(!is_dir($dossierImport)){
-			mkdir($dossierImport, 0777, true);
-		}
-		echo 'le fichier est : '.$_FILES['mon_fichier']['name'];
+
+
+	$dossier = 'import/';
+	$fichier = basename($_FILES['mon_fichier']['name']);
+	$taille_maxi = 1000000;
+	$taille = filesize($_FILES['mon_fichier']['tmp_name']);
+	$extensions = array('.txt');
+	$extension = strrchr($_FILES['mon_fichier']['name'], '.');
+	
+	//Début des vérifications de sécurité...
+	if(!in_array($extension, $extensions)) {
+		$erreur = 'Vous devez uploader un fichier txt';
 	}
+	
+	if($taille>$taille_maxi) {
+		$erreur = 'Le fichier est trop gros...';
+	}
+	
+	if(!isset($erreur))	{
+		//On formate le nom du fichier ici...
+		$fichier =  md5(uniqid(rand(), true));
+		
+	if(move_uploaded_file($_FILES['mon_fichier']['tmp_name'], $dossier . $fichier)) {
+		echo 'Upload effectué avec succès !';
+	}
+	else {
+		echo 'Echec de l\'upload !';
+	}
+}
+
+else {
+	echo $erreur;
+}
 	
 ?>
