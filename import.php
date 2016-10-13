994607fd118165c2493b12ce16a98437c262e603
messuvel <messuvel@vm-lme-dev>
2016-10-13 11:19:50 +0200

    maj import

--- import.php
+++ import.php
@@ -7,26 +7,32 @@
 		<link rel="stylesheet" href="style.css" />
 		<title>Import d'un Questionnaire</title>
 	</head>
-	
-	<?php
-		//Créer un dossier 'import'
-		$filenameImport = md5(uniqid(rand(), true));
-		$dossierImport = 'import';
-		if(!is_dir($dossierImport)){
-			mkdir($dossierImport, 0777, true);
-		}
-	?>
-	
-
+		
 	<body>
 		<h1>Importation d'un Questionnaire</h1>
+	
+		<?php
+			//Créer un dossier 'import'
+			if ($monfichier == null) {
+				$filenameImport = md5(uniqid(rand(), true));
+				$dossierImport = 'import';
+				if(!is_dir($dossierImport)){
+					mkdir($dossierImport, 0777, true);
+				}
+				print "le fichier est : $_FILES.['mon_fichier']['name']";
+			}
+			
+		?>
 		
-	<form method="post" action="import.php" enctype="multipart/form-data">
-	<label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
-	<input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
-	<input type="file" name="mon_fichier" id="mon_fichier" /><br />
-	<input type="submit" name="submit" value="Envoyer" />
-	</form>
+		
+
+		
+		<form method="post" action="import.php" enctype="multipart/form-data">
+			<label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
+			<input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+			<input type="file" name="mon_fichier" id="mon_fichier" /><br /> <br /> <br />
+			<input type="submit" name="submit" value="Envoyer" />
+		</form>
 
 
 
