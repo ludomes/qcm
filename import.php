1a35e0aa76f6a9c4bf6c92cddc8f913055356ba9
messuvel <messuvel@vm-lme-dev>
2016-10-13 10:50:39 +0200

    import v3

--- import.php
+++ import.php
@@ -5,7 +5,7 @@
 	<head>
 		<meta charset="ISO-8859-1">
 		<link rel="stylesheet" href="style.css" />
-		<title>Accueil QCM</title>
+		<title>Import d'un Questionnaire</title>
 	</head>
 	
 	<?php
@@ -18,20 +18,14 @@
 	?>
 
 	<body>
-		<h1>Accueil QCM</h1>
+		<h1>Importation d'un Questionnaire</h1>
 		
-<form method="post" action="reception.php" enctype="multipart/form-data">
-<label for="icone">Icône du fichier (JPG, PNG ou GIF | max. 15 Ko) :</label><br />
-<input type="file" name="icone" id="icone" /><br />
-<label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
-<input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
-<input type="file" name="mon_fichier" id="mon_fichier" /><br />
-<label for="titre">Titre du fichier (max. 50 caractères) :</label><br />
-<input type="text" name="titre" value="Titre du fichier" id="titre" /><br />
-<label for="description">Description de votre fichier (max. 255 caractères) :</label><br />
-<textarea name="description" id="description"></textarea><br />
-<input type="submit" name="submit" value="Envoyer" />
-</form>
+	<form method="post" action="import.php" enctype="multipart/form-data">
+	<label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
+	<input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+	<input type="file" name="mon_fichier" id="mon_fichier" /><br />
+	<input type="submit" name="submit" value="Envoyer" />
+	</form>
 
 
 
