d3ddccea2812f7140dadcbbc48c10e4b26396e20
Cyriaque MAHIETTE <cyriaque.mahiette.1989@gmail.com>
2016-10-20 10:55:35 +0200

    liste de question

--- Eleve.php
+++ Eleve.php
@@ -9,6 +9,38 @@
 	
 	<body>
 		<p><?php echo $_POST['prenom'].' '.$_POST['nom']; ?></p>
-				
+		
+		<div id="questionnaire">
+  			<ul id="onglets">
+				<?php
+					for($i = 0; $i < 20; $i++)
+					{
+						echo '<li><a href=\"'.$i.'\">'.$i.'</a></li>';
+					}
+				?>
+  			</ul>
+		</div>
+		
+		<script>
+			var minutes = 0
+			var secondes = 0
+
+			function chrono()
+			{
+				if(secondes > 59)
+				{
+					secondes = 0;
+					minutes++;
+				}
+				document.getElementById("chronotime").innerHTML = minutes + ":" + secondes
+			} 
+			timer=setInterval("chrono()", 1000);
+					
+			
+
+		</script>
+		
+		
+						
 	</body>
 </html>
