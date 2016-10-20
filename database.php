1e0da91d0a3b051b9969e2123b69e2d187dd17ed
messuvel <messuvel@vm-lme-dev>
2016-10-20 11:21:39 +0200

    maj database

--- database.php
+++ database.php
@@ -8,6 +8,7 @@ function connectDatabase () {
 	// on se connecte à example.com et au port 3307
 	$link = mysql_connect($ServeurMySQL, $UserMySQL, $UserPWD);
 	if (!$link) {
+		echo 'Connexion impossible';
 		die('Connexion impossible : ' . mysql_error());
 	}
 	echo 'Connecté correctement';
