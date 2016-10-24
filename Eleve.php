1b23c6d1a16329d9fb66c083e8dec5473dfd80e3
Cyriaque MAHIETTE <cyriaque.mahiette.1989@gmail.com>
2016-10-24 17:38:53 +0200

    test chrono

--- Eleve.php
+++ Eleve.php
@@ -6,90 +6,81 @@
 		<link rel="stylesheet" href="style.css" />
 		<title>QCM</title>
 		<script type="text/Javascript">
-            var secondes = 0;
-            var timer;
-            var pause = false;
-            var text = "";
-             
-            function IndiquerMinutes(min)
-            {
-                secondes = min * 60;
-            }
-            function Chrono()
-            {
-                if (secondes > 0)
-                {
-                    var minutes = Math.floor(secondes/60);
-                    var heures = Math.floor(minutes/60);
-                    secondes -= minutes * 60;
-                    if (heures > 0)
-                    {
-                        minutes -= heures * 60;
-                        if (minutes > 0)
-                        {
-                            text = "Il reste " + heures + ' h ' + minutes + ' min ' + secondes + ' sec';
-                        }
-                        else
-                        {  
-                            text = "Il reste " + heures + ' h ' + secondes + ' sec';
-                        }
-                        minutes = minutes + (heures * 60);
-                        secondes = secondes + (minutes * 60) - 1;
-                         
-                    }
-                    else if (minutes > 0)
-                    {
-                        text = "Il reste " + minutes + ' min ' + secondes + ' sec';
-                        secondes = secondes + (minutes * 60) - 1;
-                    }
-                    else
-                    {
-                        text = "Il reste " + secondes + ' sec';
-                        secondes = secondes + (minutes * 60) - 1;
-                    }
-                }
-                else
-                {
-                    clearInterval(timer);
-                    text = "Le temps est écoulé";
-                }
-                document.getElementById('chrono').innerHTML = text;
-            }
-            function DemarrerChrono()
-            {
-                timer = setInterval('Chrono()', 1000);
-                document.getElementById('btn_dem').style.display = 'none';
-                document.getElementById('btn_stop').style.display = 'inline';
-                document.getElementById('btn_pause').style.display = 'inline';
-                 
-            }
-            function ArreterChrono()
-            {
-                clearInterval(timer);
-                document.getElementById('btn_dem').style.display = 'inline';
-                document.getElementById('btn_stop').style.display = 'none';
-                document.getElementById('btn_pause').style.display = 'none';
-            }
-            function PauseChrono()
-            {
-                if (!pause)
-                {
-                    pause = true;
-                    clearInterval(timer);
-                    text = '[EN PAUSE] ' + text;
-                    document.getElementById('chrono').innerHTML = text;
-                    document.getElementById('btn_stop').style.display = 'none';
-                    document.getElementById('btn_pause').value = 'Reprendre';
-                }
-                else
-                {
-                    pause = false;
-                    DemarrerChrono();
-                    document.getElementById('btn_pause').value = 'Pause';
-                }
-            }
-             
-        </script>
+			var startTime = 0
+			var start = 0
+			var end = 0
+			var diff = 0
+			var timerID = 0
+			window.onload = chronoStart;
+			
+			function chrono()
+			{
+				end = new Date()
+				diff = end - start
+				diff = new Date(diff)
+				var msec = diff.getMilliseconds()
+				var sec = diff.getSeconds()
+				var min = diff.getMinutes()
+				var hr = diff.getHours()-1
+				if (min < 10)
+				{
+					min = "0" + min
+				}
+				if (sec < 10)
+				{
+					sec = "0" + sec
+				}
+				if(msec < 10)
+				{
+					msec = "00" +msec
+				}
+				else if(msec < 100)
+				{
+					msec = "0" +msec
+				}
+				document.getElementById("chronotime").value = hr + ":" + min + ":" + sec + ":" + msec
+				timerID = setTimeout("chrono()", 10)
+			}
+			
+			function chronoStart()
+			{
+				document.chronoForm.startstop.value = "stop!"
+				document.chronoForm.startstop.onclick = chronoStop
+				document.chronoForm.reset.onclick = chronoReset
+				start = new Date()
+				chrono()
+			}
+			
+			function chronoContinue()
+			{
+				document.chronoForm.startstop.value = "stop!"
+				document.chronoForm.startstop.onclick = chronoStop
+				document.chronoForm.reset.onclick = chronoReset
+				start = new Date()-diff
+				start = new Date(start)
+				chrono()
+			}
+			
+			function chronoReset()
+			{
+				document.getElementById("chronotime").value = "0:00:00:000"
+				start = new Date()
+			}
+			
+			function chronoStopReset()
+			{
+				document.getElementById("chronotime").value = "0:00:00:000"
+				document.chronoForm.startstop.onclick = chronoStart
+			}
+			
+			function chronoStop()
+			{
+				document.chronoForm.startstop.value = "start!"
+				document.chronoForm.startstop.onclick = chronoContinue
+				document.chronoForm.reset.onclick = chronoStopReset
+				clearTimeout(timerID)
+			}
+		</script>
 	</head>
 	
 	<body>
@@ -106,13 +97,10 @@
   			</ul>
 		</div>
 		
-		<form name="f_chrono">
-            <label for="saisie">Entrez le temps voulu en minutes : </label>
-            <input type="text" name="saisie" style="text-align: right;" /><br />
-            <input type="button" name="btn_dem" id="btn_dem" value="Démarrer" onclick="IndiquerMinutes(f_chrono.saisie.value); DemarrerChrono();" />
-            <input type="button" name="btn_stop" id="btn_stop" value="Arreter !" onclick="ArreterChrono();" style="display: none;" />
-            <input type="button" name="btn_pause" id="btn_pause" value="Pause" onclick="PauseChrono();" style="display: none;" />
-        </form>
-        <p id="chrono"></p>						
+		<form name="chronoForm">
+ 			<input type="text" name="chronotime" id="chronotime" value="0:00:00:00"/>
+    		<input type="button" name="startstop" value="start!" onClick="chronoStart()" />
+    		<input type="button" name="reset" value="reset!" onClick="chronoReset()" />
+		</form>						
 	</body>
 </html>
