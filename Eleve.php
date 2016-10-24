96f8da8094df7cf22c9c280120aef1219ecf84b9
Cyriaque MAHIETTE <cyriaque.mahiette.1989@gmail.com>
2016-10-24 12:19:37 +0200

    test chrono

--- Eleve.php
+++ Eleve.php
@@ -22,22 +22,38 @@
 		</div>
 		
 		<script>
-			var minutes = 0
-			var secondes = 0
+			var centi=0 // initialise les dixtièmes
+			var secon=0 //initialise les secondes
+			var minu=0 //initialise les minutes
 
 			function chrono()
 			{
-				if(secondes > 59)
+				centi++; //incrémentation des dixièmes de 1
+				if(centi>9)
 				{
-					secondes = 0;
-					minutes++;
-				}
-				document.getElementById('chrono').innerHTML = text;
+					centi=0;secon++
+				} //si les dixièmes > 9, on les réinitialise à 0 et on incrémente les secondes de 1
+				if(secon>59)
+				{
+					secon=0;minu++
+				} //si les secondes > 59, on les réinitialise à 0 et on incrémente les minutes de 1
+				document.forsec.secc.value=" "+centi //on affiche les dixièmes
+				document.forsec.seca.value=" "+secon //on affiche les secondes
+				document.forsec.secb.value=" "+minu //on affiche les minutes
+				compte=setTimeout('chrono()',100) //la fonction est relancée tous les 10° de secondes
 			}
-			timer = setInterval('chrono()', 1000);
-		</script>
-		
-		
-						
+
+			function rasee()
+			{ //fonction qui remet les compteurs à 0
+				clearTimeout(compte) //arrête la fonction chrono()
+				centi=0;
+				secon=0;
+				minu=0;
+				document.forsec.secc.value=" "+centi
+				document.forsec.seca.value=" "+secon
+				document.forsec.secb.value=" "+minu
+			}
+
+		</script>						
 	</body>
 </html>
