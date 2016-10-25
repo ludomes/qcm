3e4af46311ebd42fece079beb506c0b408235d43
messuvel <messuvel@vm-lme-dev>
2016-10-25 17:00:32 +0200

    maj

--- DBMySql.php
+++ DBMySql.php
@@ -17,14 +17,6 @@ class DBMySql {
 		}
 		echo 'Connecté correctement';
 		mysql_close($link);
-		
-		// on se connect à localhost au port 3307
-		$link = mysql_connect('127.0.0.1:3307', 'mysql_user', 'mysql_password');
-		if (!$link) {
-			die('Connexion impossible : ' . mysql_error());
-		}
-		echo 'Connecté correctement';
-		mysql_close($link);
 	}
 }
 
