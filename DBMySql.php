da60b2e1bf0b704f09699bf83077a5aefc2f91bd
messuvel <messuvel@vm-lme-dev>
2016-10-25 17:58:28 +0200

    maj

--- DBMySql.php
+++ DBMySql.php
@@ -3,29 +3,30 @@
 
 
 class DBMySql {
-	private $ServeurMySQL;
-	private $UserMySQL;
-	private $UserPWD;
+	private $_ServeurMySQL;
+	private $_UserMySQL;
+	private $_UserPWD;
+	private $_link;
 	
     function __construct() {
-    	$ServeurMySQL= 'localhost';
-    	$UserMySQL = 'ludomess';
-    	$UserPWD = 'ludomess';
+    	$this->_ServeurMySQL= 'localhost';
+    	$this->_UserMySQL = 'ludomess';
+    	$this->_UserPWD = 'ludomess';
     }
 	
 	public function connectDatabase  () {
 		// on se connecte à example.com et au port 3307
 		echo 'Connexion au serveur sql <br>';
-		echo 'User : ' . $UserMySQL . '<br>';
-		$link = mysql_connect($ServeurMySQL, $UserMySQL, $UserPWD);
-		if (!$link) {
+		echo 'User : ' . $this->UserMySQL . '<br>';
+		$this->link = mysql_connect($this->ServeurMySQL, $this->UserMySQL, $this->UserPWD);
+		if (!$this->link) {
 			die('Connexion impossible : ' . mysql_error());
 		}
 		echo 'Connecté correctement';
 	}
 	
 	public function disconnectDatabase () {
-		mysql_close($link);
+		mysql_close($this->link);
 	}
 }
 
